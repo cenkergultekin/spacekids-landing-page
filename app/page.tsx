da840acac0307f8bbc4e8450bf41c8350fdd795d
ardaongl <ongul357@gmail.com>
2025-08-27 04:29:05 +0300

    sayfa yapısında değişiklikler yapıldı

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,9 @@ export default function Home() {
       <Header />
       <HeroSection />
       <ModernCardsSection />
+      <CustomerSpotlight />
       <PlatformShowcase />
       <WorkforceManagement />
-      <CustomerSpotlight />
       <StatisticsSection />
       <FAQSection />
       <Footer />
